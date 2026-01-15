5fac690d0f280e4a92f46a3f84c159764ff1d75c
AliRaza <alirq.dev@gmail.com>
2026-01-15 18:29:11 -0500

    Dark Theme worked upon

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let time = document.querySelector(".time");
-
+let darkBtn = document.querySelector("#darkTheme");
+let body = document.body;
 setInterval(()=> {
-    time.textContent = new Date().toLocaleTimeString()
+    time.textContent = new Date().toLocaleTimeString("en-US", {hour12:true})
 }, 1000
 )
+
+darkBtn.addEventListener(() => {
+    body.classList.add("dark");
+    darkBtn.textContent = "☀️";
+})
